7b9636f9468014f5c91e3437caf6d0c082f3aa50
Dheenadhayalan211203 <dheenashobha2k3@gmail.com>
2024-07-30 22:36:10 +0530

    first commit

---
+++ Server.cjs
@@ -0,0 +1,51 @@
+const express=require('express');
+ 
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json())
+const PORT = 3000;
+require("dotenv").config();
+
+
+const MONGODB_URI="mongodb+srv:Dheena:dheena@cluster0.ser6ewc.mongodb.net/chatogram?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  mailid:  {type:String,require:true},
+  password: {type:String,require:true},
+  confirmpassword: {type:String,require:true},
+  name: {type:String,require:true},
+  phone: {type:String,require:true}
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.post("/signup", async (req, res) => {
+  const { mailid, password, confirmpassword, name, phone } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ mailid });
+    if (existingUser) {
+      return res.status(400).send("User already exists");
+    }
+
+    const user = new User({ mailid, password, confirmpassword, name, phone });
+    await user.save();
+
+    res.status(201).send("User created successfully");
+  } catch (error) {
+    res.status(500).send("Error creating user");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
